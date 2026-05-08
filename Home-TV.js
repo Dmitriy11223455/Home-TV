10e6be5e73d7b485284a68c41a3618276567926c
Dmitriy11223455 <vbarishnik50@gmail.com>
2026-05-08 20:40:52 +0300

    Update Home-TV.js

--- Home-TV.js
+++ Home-TV.js
@@ -1,35 +1,74 @@
 (function () {
     'use strict';
 
-    function start() {
-        // Уведомление, чтобы мы увидели, что плагин вообще запустился
-        Lampa.Noty.show('Плагин ТВ загружается...');
+    function startPlugin() {
+        // Уведомление при загрузке (чтобы знать, что плагин ожил)
+        Lampa.Noty.show('Плагин ТВ запущен');
 
-        Lampa.Component.add('my_tv_final', function (object, exam) {
+        Lampa.Component.add('my_custom_tv', function (object, exam) {
             var network = new Lampa.Reguest();
-            var scroll = new Lampa.Scroll({mask: true, over: true});
-            var items = [];
-            var html = $('<div class="category-full"><div class="category-full__body"></div></div>');
-            var body = html.find('.category-full__body');
+            var scroll  = new Lampa.Scroll({mask: true, over: true});
+            var items   = [];
+            var html    = $('<div class="category-full"></div>');
+            var body    = $('<div class="category-full__body"></div>');
 
-            var channels = [
-                {title: 'Первый канал', url: 'https://google.com', regex: /(.*)/, img: ''}
+            // --- ТВОЙ СПИСОК КАНАЛОВ ---
+            var myChannels = [
+                {
+                    title: 'Первый канал',
+                    url: 'https://site-a.com', 
+                    regex: /(https?:\/\/[^"']+\.m3u8[^"']*)/i,
+                    img: 'https://nocookie.net'
+                },
+                {
+                    title: 'ТНТ',
+                    url: 'https://site-b.net',
+                    regex: /file:"(.*?\.m3u8)"/i,
+                    img: ''
+                }
             ];
 
             this.create = function () {
                 var _this = this;
-                channels.forEach(function (item) {
-                    var card = Lampa.Template.get('card', {title: item.title, release_year: 'TV'});
-                    card.on('hover:enter', function () {
-                        Lampa.Noty.show('Работает!');
+                myChannels.forEach(function (item) {
+                    // Создаем карточку как в Кулике
+                    var card = Lampa.Template.get('card', {
+                        title: item.title,
+                        release_year: 'TV'
                     });
+                    
+                    if(item.img) card.find('.card__img').attr('src', item.img);
+
+                    card.on('hover:enter', function () {
+                        Lampa.Noty.show('Поиск потока...');
+                        
+                        // Используем native для обхода ограничений
+                        network.native(item.url, function (response) {
+                            var found = response.match(item.regex);
+                            if (found) {
+                                var streamUrl = found[1] ? found[1] : found[0];
+                                Lampa.Player.play({
+                                    url: streamUrl.replace(/"/g, ''),
+                                    title: item.title
+                                });
+                            } else {
+                                Lampa.Noty.error('Ссылка не найдена');
+                            }
+                        }, function () {
+                            Lampa.Noty.error('Сайт не отвечает');
+                        }, false, {dataType: 'text'});
+                    });
+                    
                     body.append(card);
                     items.push(card);
                 });
+
+                html.append(body);
                 scroll.append(html);
             };
 
             this.render = function () { return scroll.render(); };
+
             this.active = function () {
                 Lampa.Controller.add('content', {
                     toggle: function () {
@@ -43,19 +82,21 @@
             };
         });
 
-        // Добавляем пункт в меню с задержкой 2 секунды (чтобы меню успело прогрузиться)
+        // Добавление в меню (через 1 сек, чтобы меню успело прогрузиться)
         setTimeout(function () {
-            var menu_item = $('<li class="menu__item selector"><div class="menu__ico"><svg viewBox="0 0 24 24" fill="white"><path d="M21 7L12 2L3 7V17L12 22L21 17V7Z"/></svg></div><div class="menu__text">TV КАНАЛЫ</div></li>');
+            var menu_item = $('<li class="menu__item selector">' +
+                '<div class="menu__ico"><svg viewBox="0 0 24 24" fill="white"><path d="M21 7L12 2L3 7V17L12 22L21 17V7Z"/></svg></div>' +
+                '<div class="menu__text">МОЁ ТВ</div>' +
+                '</li>');
+
             menu_item.on('hover:enter', function () {
-                Lampa.Activity.push({title: 'ТВ', component: 'my_tv_final'});
+                Lampa.Activity.push({ title: 'Телевидение', component: 'my_custom_tv' });
             });
             $('.menu .menu__list').append(menu_item);
-            console.log('Plugin: Menu item added');
-        }, 2000);
+        }, 1000);
     }
 
-    // Ждем готовности приложения
-    if (window.appready) start();
-    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') start(); });
+    if (window.appready) startPlugin();
+    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
 })();
 

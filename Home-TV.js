1f460580aaf321c23b35f367f66023e08204905a
Dmitriy11223455 <vbarishnik50@gmail.com>
2026-05-08 20:35:07 +0300

    Update Home-TV.js

--- Home-TV.js
+++ Home-TV.js
@@ -2,63 +2,65 @@
     'use strict';
 
     function startPlugin() {
-        Lampa.Component.add('hybrid_plugin', function (object, exam) {
+        Lampa.Component.add('my_kulik_plugin', function (object, exam) {
             var network = new Lampa.Reguest();
-            var scroll = new Lampa.Scroll({mask:true, over:true});
+            var scroll = new Lampa.Scroll({mask: true, over: true});
             var items = [];
             var html = $('<div class="category-full"></div>');
+            var body = $('<div class="category-full__body"></div>');
 
-            var sources = [
+            // --- ТВОИ КАНАЛЫ (как в Кулик ТВ) ---
+            var myChannels = [
                 {
-                    name: 'Sweet TV',
-                    base_url: 'https://sweet-tv.net',
-                    regex: /(https?:\/\/[^"']+\.m3u8[^"']*)/i, 
-                    channels: [
-                        { title: 'Россия 1', path: '/rossia-1.html' },
-                        { title: 'НТВ', path: '/page2.html' }
-                    ]
+                    title: 'Первый канал',
+                    url: 'https://site-a.com',
+                    regex: /(https?:\/\/[^"']+\.m3u8[^"']*)/i,
+                    img: 'https://nocookie.net'
                 },
                 {
-                    name: 'Сайт Б',
-                    base_url: 'https://site-b.net',
-                    regex: /file:"(.*?\.m3u8)"/i, 
-                    channels: [
-                        { title: 'ТНТ', path: '/tnt-online' },
-                        { title: 'СТС', path: '/ctc-online' }
-                    ]
+                    title: 'ТНТ',
+                    url: 'https://site-b.net',
+                    regex: /file:"(.*?\.m3u8)"/i,
+                    img: ''
                 }
             ];
 
             this.create = function () {
                 var _this = this;
-                sources.forEach(function (source) {
-                    var head = $('<div class="category__title">' + source.name + '</div>');
-                    html.append(head);
-
-                    source.channels.forEach(function (channel) {
-                        var card = Lampa.Template.get('button_item', {title: channel.title});
-                        card.on('hover:enter', function () {
-                            Lampa.Noty.show('Запрос к ' + source.name);
-                            var fullUrl = source.base_url + channel.path;
-                            network.native(fullUrl, function (response) {
-                                var found = response.match(source.regex);
-                                if (found) {
-                                    var streamUrl = found[1] ? found[1] : found[0];
-                                    Lampa.Player.play({
-                                        url: streamUrl.replace(/"/g, ''),
-                                        title: channel.title
-                                    });
-                                } else {
-                                    Lampa.Noty.error('Ссылка не найдена');
-                                }
-                            }, function () {
-                                Lampa.Noty.error('Сайт не отвечает');
-                            }, false, {dataType: 'text'});
-                        });
-                        html.append(card);
-                        items.push(card);
+                myChannels.forEach(function (item) {
+                    // Создаем плитку (карточку) как в Кулике
+                    var card = Lampa.Template.get('card', {
+                        title: item.title,
+                        release_year: 'TV'
                     });
+                    
+                    // Устанавливаем логотип
+                    if(item.img) card.find('.card__img').attr('src', item.img);
+
+                    card.on('hover:enter', function () {
+                        Lampa.Noty.show('Поиск потока...');
+                        
+                        network.native(item.url, function (response) {
+                            var found = response.match(item.regex);
+                            if (found) {
+                                var stream = found[1] ? found[1] : found[0];
+                                Lampa.Player.play({
+                                    url: stream.replace(/"/g, ''),
+                                    title: item.title
+                                });
+                            } else {
+                                Lampa.Noty.error('Ссылка не найдена');
+                            }
+                        }, function () {
+                            Lampa.Noty.error('Сайт не отвечает');
+                        }, false, {dataType: 'text'});
+                    });
+                    
+                    body.append(card);
+                    items.push(card);
                 });
+
+                html.append(body);
                 scroll.append(html);
             };
 
@@ -79,13 +81,13 @@
 
         Lampa.Listener.follow('app', function (e) {
             if (e.type == 'ready') {
-                var menu_item = $('<li class="menu__item selector" data-action="hybrid">' +
-                    '<div class="menu__ico"><svg viewBox="0 0 24 24" fill="white"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14h-2v-4H8v-2h4V7h2v4h4v2h-4v4z"/></svg></div>' +
-                    '<div class="menu__text">Гибрид ТВ</div>' +
+                var menu_item = $('<li class="menu__item selector">' +
+                    '<div class="menu__ico"><svg viewBox="0 0 24 24" fill="white"><path d="M21 7L12 2L3 7V17L12 22L21 17V7Z"/></svg></div>' +
+                    '<div class="menu__text">Мой Кулик</div>' +
                     '</li>');
 
                 menu_item.on('hover:enter', function () {
-                    Lampa.Activity.push({ title: 'Каналы', component: 'hybrid_plugin' });
+                    Lampa.Activity.push({ title: 'ТВ Каналы', component: 'my_kulik_plugin' });
                 });
                 $('.menu .menu__list').append(menu_item);
             }
@@ -95,3 +97,4 @@
     if (window.appready) startPlugin();
     else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
 })();
+

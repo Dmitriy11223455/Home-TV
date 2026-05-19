3d7979725a00c63ccd0a5a1719eccf17db2fcc02
Kalnev <vbarishnik50@gmail.com>
2026-05-19 16:18:54 +0300

    Update Home-TV.js

--- Home-TV.js
+++ Home-TV.js
@@ -81,7 +81,6 @@
                                     url: streamUrl.split('|')[0], 
                                     title: channel.title,
                                     headers: {
-                                        'Referer': 'https://mediavitrina.ru',
                                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
                                     }
                                 });
